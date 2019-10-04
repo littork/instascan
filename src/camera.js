0a65f98fae5540deaeec1cadfccb7d3357650100
Littork <littork@gmail.com>
2019-10-04 17:15:38 -0600

    Update to support ios, update to fix double permission request issue

--- src/camera.js
+++ src/camera.js
@@ -1,84 +1,88 @@
 function cameraName(label) {
-  let clean = label.replace(/\s*\([0-9a-f]+(:[0-9a-f]+)?\)\s*$/, '');
-  return clean || label || null;
+    let clean = label.replace(/\s*\([0-9a-f]+(:[0-9a-f]+)?\)\s*$/, "");
+    return clean || label || null;
 }
 
 class MediaError extends Error {
-  constructor(type) {
-    super(`Cannot access video stream (${type}).`);
-    this.type = type;
-  }
+    constructor(type) {
+        super(`Cannot access video stream (${type}).`);
+        this.type = type;
+    }
 }
 
 class Camera {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
-    this._stream = null;
-  }
-
-  async start() {
-    let constraints = {
-      audio: false,
-      video: {
-        mandatory: {
-          sourceId: this.id,
-          minWidth: 600,
-          maxWidth: 800,
-          minAspectRatio: 1.6
-        },
-        optional: []
-      }
-    };
-
-    this._stream = await Camera._wrapErrors(async () => {
-      return await navigator.mediaDevices.getUserMedia(constraints);
-    });
-
-    return this._stream;
-  }
-
-  stop() {
-    if (!this._stream) {
-      return;
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this._stream = null;
     }
 
-    for (let stream of this._stream.getVideoTracks()) {
-      stream.stop();
+    async start() {
+        let constraints = {
+            audio: false,
+            video: {
+                mandatory: {
+                    sourceId: this.id,
+                    minWidth: 600,
+                    maxWidth: 800,
+                    minAspectRatio: 1.6
+                },
+                optional: []
+            }
+        };
+
+        if (/iP(hone|(o|a)d)/.test(navigator.userAgent)) {
+            constraints.video.facingMode = { exact: "environment" };
+        }
+
+        this._stream = await Camera._wrapErrors(async () => {
+            return await navigator.mediaDevices.getUserMedia(constraints);
+        });
+
+        return this._stream;
     }
 
-    this._stream = null;
-  }
+    stop() {
+        if (!this._stream) {
+            return;
+        }
 
-  static async getCameras() {
-    await this._ensureAccess();
+        for (let stream of this._stream.getVideoTracks()) {
+            stream.stop();
+        }
 
-    let devices = await navigator.mediaDevices.enumerateDevices();
-    return devices
-      .filter(d => d.kind === 'videoinput')
-      .map(d => new Camera(d.deviceId, cameraName(d.label)));
-  }
-
-  static async _ensureAccess() {
-    return await this._wrapErrors(async () => {
-      let access = await navigator.mediaDevices.getUserMedia({ video: true });
-      for (let stream of access.getVideoTracks()) {
-        stream.stop();
-      }
-    });
-  }
-
-  static async _wrapErrors(fn) {
-    try {
-      return await fn();
-    } catch (e) {
-      if (e.name) {
-        throw new MediaError(e.name);
-      } else {
-        throw e;
-      }
+        this._stream = null;
     }
-  }
+
+    static async getCameras() {
+        let devices = await navigator.mediaDevices.enumerateDevices();
+        return devices
+            .filter(d => d.kind === "videoinput")
+            .map(d => new Camera(d.deviceId, cameraName(d.label)));
+    }
+
+    static async _ensureAccess() {
+        return await this._wrapErrors(async () => {
+            let access = await navigator.mediaDevices.getUserMedia({
+                video: true
+            });
+            for (let stream of access.getVideoTracks()) {
+                stream.stop();
+            }
+        });
+    }
+
+    static async _wrapErrors(fn) {
+        try {
+            return await fn();
+        } catch (e) {
+            if (e.name) {
+                throw new MediaError(e.name);
+            } else {
+                throw e;
+            }
+        }
+    }
 }
 
 module.exports = Camera;
